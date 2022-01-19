11e11d4fa6b2655fcc45a72fd032c9fefc8584c9
smuppirishet <96856460+swethamuppirishetty@users.noreply.github.com>
2022-01-19 14:33:27 +0530

    maven files

---
+++ learning-mgmt-system/src/main/java/com/dxc/learning/Batch.java
@@ -0,0 +1,122 @@
+package com.dxc.learning;
+
+class Employee{
+    private int id;
+    private String name;
+    private String Address;
+    private String email;
+    private String track;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTrack() {
+        return track;
+    }
+    public void setTrack(String track) {
+        this.track = track;
+    }
+    
+    
+}
+  
+
+class Mentor{
+    private int id;
+    private String name;
+    private String Address;
+    private String email;
+    private String specialization;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSpecialization() {
+        return specialization;
+    }
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+
+}
+ 
+
+public class Batch{
+    private int id;
+    private String name;
+    private date startDate;
+    private date endDate;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(date startDate) {
+        this.startDate = startDate;
+    }
+    public date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(date endDate) {
+        this.endDate = endDate;
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
---
+++ learning-mgmt-system/src/main/java/com/dxc/learning/date.java
@@ -0,0 +1,5 @@
+package com.dxc.learning;
+
+public class date {
+
+}
